69435c8cd2aa8dbf773bea0d9ef7d257a4b5acb5
zag2000 <38731858+zag2000@users.noreply.github.com>
2018-04-25 15:22:19 -0400

    Update 612_Project.py

--- 612_Project.py
+++ 612_Project.py
@@ -5,9 +5,6 @@ Created on Tue Apr 24 20:37:04 2018
 """
 
 
-#import csv
-#from urllib.request import urlopen
-#from bs4 import BeautifulSoup
 import argparse
 
 parser = argparse.ArgumentParser(description='Takes in filters for craigslist search (exclusively for motorcycles and cars)')
@@ -29,95 +26,107 @@ parser.add_argument("-ti", "--title", help="Title status (Options: clean, salvag
 parser.add_argument("-tr", "--trans", help="Transmission (Options: manual, automatic, other) (default: None)", action="store", dest="trans", default=None)
 parser.add_argument("-l", "--location", help="Location of the item (default: None)", action="store", dest="location", default=None)
 results = parser.parse_args()
+
 baseUrl = 'https://boston.craigslist.org/search/sss?sort=rel'
 
 #print("results: " + str(results))
-location=''
+
+baseUrl = 'https://boston.craigslist.org/search/sss?sort=rel'
+
+#print("results: " + str(results))
 miles=''
+postal=''
+min_price=''
+max_price=''
 make=''
 model=''
+min_eng_disp=''
+max_eng_disp=''
+min_year=''
+max_year=''
+min_odo=''
+max_odo=''
+condition=''
+fuel=''
+color=''
+title=''
+trans=''
+location=''
 
 if results.miles:
-    location= "&postal=" + str(results.location)
+    location= "&search_distance=" + str(results.location)
     baseUrl = baseUrl + location
-if results.zip:
-    miles= "&search_distance=" + str(results.miles)
+    
+if results.postal:
+    miles= "&postal=" + str(results.miles)
     baseUrl = baseUrl + miles
+    
 if results.min_price:
-    location= "&postal=" + str(results.location)
+    location= "&min_price=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.max_price:
-    miles= "&search_distance=" + str(results.miles)
+    miles= "&max_price=" + str(results.miles)
     baseUrl = baseUrl + miles
+    
 if results.make:
-    location= "&postal=" + str(results.location)
+    location= "&auto_make_model=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.model:
-    miles= "&search_distance=" + str(results.miles)
+    miles= "&postal=" + str(results.miles)
     baseUrl = baseUrl + miles
+    
 if results.min_eng_disp:
-    location= "&postal=" + str(results.location)
+    location= "&min_engine_displacement_cc=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.max_eng_disp:
-    miles= "&search_distance=" + str(results.miles)
+    miles= "&max_engine_displacement_cc=" + str(results.miles)
     baseUrl = baseUrl + miles
+    
 if results.min_year:
-    location= "&postal=" + str(results.location)
+    location= "&min_auto_year=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.max_year:
-    location= "&postal=" + str(results.location)
+    location= "&max_auto_year=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.min_odo:
-    location= "&postal=" + str(results.location)
+    location= "&min_auto_miles=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.max_odo:
-    location= "&postal=" + str(results.location)
+    location= "&max_auto_miles=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.condition:
-    location= "&postal=" + str(results.location)
+    location= "&condition=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.fuel:
-    location= "&postal=" + str(results.location)
+    location= "&auto_fuel_type=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.color:
-    location= "&postal=" + str(results.location)
+    location= "&auto_paint=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.title:
-    location= "&postal=" + str(results.location)
+    location= "&auto_title_status=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.trans:
-    location= "&postal=" + str(results.location)
+    location= "&auto_transmission=" + str(results.location)
     baseUrl = baseUrl + location
+    
 if results.location:
     location= "&postal=" + str(results.location)
     baseUrl = baseUrl + location
 
   
-print(baseUrl)    
+print(baseUrl)        
+  
+
     
-#mylist = []
-#
-#html = urlopen("https://nh.craigslist.org/search/mca")
-#
-#soup = BeautifulSoup(html, "html.parser")
-#
-#for x in soup.find_all("li", {"class":"result-row"})[1:]:
-#    
-#    for n in x.find_all("time", {"class":"result-date"}):
-#        time = (n['title'])
-#        
-#    for a in x.find_all('a', {"class":"result-title"}, href=True):
-#        href = a['href']
-#    a = x.find_all("a")
-#    data = time, a[1].text, "https://nh.craigslist.org"+href
-#    mylist.append(data)
-#    
-#for i in mylist:
-#    pass
-#    #print(i, "\n")
-#
-#with open('Boston area.csv', 'a') as outcsv:   
-#    writer = csv.writer(outcsv, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-#    for item in mylist:
-#        writer.writerow(item)
-#    
